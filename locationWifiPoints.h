757a6ab24378e07e2d8a849745d1850ed8cd7ba9
sgrfazli <1rezartabriz@gmail.com>
2023-03-09 16:01:35 +0000

    add locationWifiPoints

---
+++ locationWifiPoints.h
@@ -0,0 +1,18 @@
+//
+// Created by sgrfazli on 09/03/2023.
+//
+
+#ifndef LOCATIONSERVICE_CPP_LOCATIONWIFIPOINTS_H
+#define LOCATIONSERVICE_CPP_LOCATIONWIFIPOINTS_H
+#include <iostream>
+#include <cstring>
+#include <string>
+#include "cJSON.h"
+#include "curl\curl.h"
+
+class locationWifiPoints {
+
+};
+
+
+#endif //LOCATIONSERVICE_CPP_LOCATIONWIFIPOINTS_H
